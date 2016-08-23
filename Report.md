7357eddafc28643d3e333037142c452a6407d4bc
SaltumDis <varatinskaite.adelina@gmail.com>
2016-08-23 15:17:01 +0200

    fixes+unit tests

--- Report.md
+++ Report.md
@@ -11,27 +11,36 @@ Week2:
 - Refactored Dependencies tree
 - Created fileWriter/Reader
 - Added comments, other fixes after review
-- Converted main cheetah expresions to jinja2
+- Converted main cheetah expressions to jinja2
 - Testing
 
 Week3:
 - Testing
 - Rewrote placeholder conversion
 - Read about regex(recursive, conditionals etc.) - tested them out
-- Created IrrevertableData to store info about cheetah expresions that can't be converted
+- Created IrrevertableData to store info about cheetah expressions that can't be converted
 - Fixed some minor bugs(search for expr. end till the end of file, continue and etc.)
 
 Week4:
-- Reconfigured VM. Fixed the probels I had before. It's up and running.
+- Reconfigured VM. Fixed the problems I had before. It's up and running.
 - Added more documentation for template converter.
-- Created scrip that analyzes any python project and returs unsupported modules/libs.
+- Created scrip that analyzes any python project and returns unsupported modules/libs.
 - Some bug fixes (changed how comments are handled, new line removal and etc.).
 
-Week5-6:
+Week5-6:  
 - Fixed manualResults output for Converter.
 - Updated scipt that analyzes any python project (Uses requests now)
 - Analyzed all projects in DMWM
 - Made list of python modules that need to be ported
 - Read https://twiki.cern.ch/twiki/bin/viewauth/CMS/DMWM
 - Researched possible libraries for migration
-- Wrote some small scripts for reserch (https://github.com/SaltumDis/conversions)
+- Wrote some small scripts for research (https://github.com/SaltumDis/conversions)
+
+Week7:  
+- Finalized report for migrations
+- Small fixes for conversions cheetah > Jinja2
+- Started migrating mox. Found out that the code that imports it is obsolete.
+- Started to research obsolete code (wrote some scripts for files comparison)
+
+Week8:  
+- Added unittest for converted
---
+++ conversion/TemplateConverterTests.py
@@ -0,0 +1,154 @@
+import unittest
+
+from TemplateConverter import TemplateConverter
+
+
+class TestConverter(unittest.TestCase):
+    def test_multiplePlaceHolders(self):
+        lines = ["$from_res&#8212;$to_res records out of $nrows.\n"]
+        converter = TemplateConverter(lines, "test_notPlaceholder")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{{from_res}}&#8212;{{to_res}} records out of {{nrows}}.\n", convertedLines[0])
+
+    def test_multiplePlaceHolders1(self):
+        lines = ["$quote($jsoncode)"]
+        converter = TemplateConverter(lines, "test_placeholderFuncion")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{{quote(jsoncode)}}", convertedLines[0])
+
+    def test_multiplePlaceHolders2(self):
+        lines = ["<td><a href='$base/task?id=$quote_plus($parent)'>$quote($parent)</a></td>"]
+        converter = TemplateConverter(lines, "test_notPlaceholder")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("<td><a href='{{base}}/task?id={{quote_plus(parent)}}'>{{quote(parent)}}</a></td>",
+                         convertedLines[0])
+
+    def test_multiplePlaceHolders3(self):
+        lines = ["$quote($jsoncode[$li[a(1)]])"]
+        converter = TemplateConverter(lines, "test_placeholderFuncion")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{{quote(jsoncode[li[a(1)]])}}", convertedLines[0])
+
+    def test_multiplePlaceHolders4(self):
+        lines = ["$quote($jsoncode[1]) aa $jsoncode[1]. $jsoncode_a($quote($la))"]
+        converter = TemplateConverter(lines, "test_placeholderFuncion")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{{quote(jsoncode[1])}} aa {{jsoncode[1]}}. {{jsoncode_a(quote(la))}}", convertedLines[0])
+
+
+    def test_placeholderFuncion(self):
+        lines = ["$qoute($stuff.morestuff(anmore()))\n"]
+        converter = TemplateConverter(lines, "test_placeholderFuncion")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{{qoute(stuff.morestuff(anmore()))}}\n", convertedLines[0])
+
+    def test_placeholderFuncion2(self):
+        lines = ["<li>$quote($key): $quote($val)</li>"]
+        converter = TemplateConverter(lines, "test_placeholderFuncion")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("<li>{{quote(key)}}: {{quote(val)}}</li>", convertedLines[0])
+
+
+    def test_set(self):
+        lines = ["#set timestamp = $time.strftime(\"%a, %d %b %Y %H:%M:%S GMT\", $time.gmtime())"]
+        converter = TemplateConverter(lines, "test_set")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{%- set timestamp = time.strftime(\"%a, %d %b %Y %H:%M:%S GMT\", time.gmtime()) %}",
+                         convertedLines[0])
+
+    def test_placeholderAsHref(self):
+        lines = ["<a href=\"$newUrl\">next</a> |"]
+        converter = TemplateConverter(lines, "test_placeholderAsHref")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("<a href=\"{{newUrl}}\">next</a> |", convertedLines[0])
+
+    def test_placeholdersKeywords(self):
+        lines = ["$quote($json.dumps($result, indent=4, default=str))"]
+        converter = TemplateConverter(lines, "test_placeholderAsHref")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{{quote(json.dumps(result, indent=4, default=str))}}", convertedLines[0])
+
+    def test_placeholdersMultiple(self):
+        lines = ["<b>$das</b> $quote($row.get(\"description\", \"N/A\"))"]
+        converter = TemplateConverter(lines, "test_placeholdersMultiple")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("<b>{{das}}</b> {{quote(row.get(\"description\", \"N/A\"))}}", convertedLines[0])
+
+    def test_comments(self):
+        lines = ["##<div><h3>Result $quote($result.get($task_id))</h3></div>\n"]
+        converter = TemplateConverter(lines, "test_comments")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{#-<div><h3>Result {{quote(result.get(task_id))}}</h3></div> #}\n", convertedLines[0])
+
+    def test_longComment(self):
+        lines = ["#####if $lfn!=$lfnList[-1]\n"]
+        converter = TemplateConverter(lines, "test_longComment")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{#-if {{lfn}}!={{lfnList[-1]}} #}\n", convertedLines[0])
+
+    def test_specChars(self):
+        lines = ["\\$('$highlight').addClassName('box_attention').show()", "\\#slide_cards span {"]
+        converter = TemplateConverter(lines, "test_specChars")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("$('$highlight').addClassName('box_attention').show()", convertedLines[0])
+        self.assertEqual("#slide_cards span {", convertedLines[1])
+
+    def test_for(self):
+        lines = ["#for row in $daskeys"]
+        converter = TemplateConverter(lines, "test_for")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{% for row in daskeys -%}", convertedLines[0])
+
+    def test_ifBlock(self):
+        lines = [
+            "#if $dbs==$dbs_global",
+            "#set msg=\"<b>default DBS instance</b>\"",
+            "#elif $dbs.startswith('prod')",
+            "#set msg=\"<em>production DBS instance</em>\"",
+            "#elif $dbs.startswith('int')",
+            "#set msg=\"<em>integration DBS instance</em>\"",
+            "#elif $dbs.startswith('dev')",
+            "#set msg=\"<em>development DBS instance</em>\"",
+            "#else",
+            "#set msg=\"\"",
+            "#end if",
+        ]
+        linesResult = [
+            "{% if dbs==dbs_global -%}",
+            "{%- set msg=\"<b>default DBS instance</b>\" %}",
+            "{% elif dbs.startswith('prod') %}",
+            "{%- set msg=\"<em>production DBS instance</em>\" %}",
+            "{% elif dbs.startswith('int') %}",
+            "{%- set msg=\"<em>integration DBS instance</em>\" %}",
+            "{% elif dbs.startswith('dev') %}",
+            "{%- set msg=\"<em>development DBS instance</em>\" %}",
+            "{% else -%}",
+            "{%- set msg=\"\" %}",
+            "{%- endif -%}",
+        ]
+        converter = TemplateConverter(lines, "test_ifBlock")
+        convertedLines = converter.getFileLines()
+        self.assertEqual(linesResult, convertedLines)
+
+    def test_silent(self):
+        lines = ["#silent $init_dbses.remove($inst)"]
+        converter = TemplateConverter(lines, "test_silent")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{{- \"\" if  init_dbses.remove(inst)}}", convertedLines[0])
+
+    def test_str(self):
+        lines = ["#set newUrl = $url + \"&amp;idx=\" + $str($last) + \"&amp;limit=\" + $str($limit)"]
+        converter = TemplateConverter(lines, "test_str")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{%- set newUrl = url + \"&amp;idx=\" + last| string + \"&amp;limit=\" + limit| string %}",
+                         convertedLines[0])
+
+    def test_lenRange(self):
+        lines = ["#for idx in $range(0, len($cards))"]
+        converter = TemplateConverter(lines, "test_lenRange")
+        convertedLines = converter.getFileLines()
+        self.assertEqual("{% for idx in range(0, cards| count) -%}", convertedLines[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
